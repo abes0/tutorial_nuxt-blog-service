e8f617aea824e1944ec7c5ce64ec0b60ba7064f5
Shinya <kangunshinu@gmail.com>
2021-05-27 16:36:23 +0900

    first commit

---
+++ app/layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <TheHeader />
+    <div class="wrapper">
+      <nuxt class="container" />
+    </div>
+  </div>
+</template>
+<script>
+import TheHeader from '~/components/TheHeader'
+export default {
+  components: {
+    TheHeader
+  }
+}
+</script>
